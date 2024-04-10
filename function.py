90a3229f804e1245e372b4fb8354fce5c829924e
Jeevithan <117728163+jeevithan003@users.noreply.github.com>
2024-04-10 19:59:32 +0530

    Create function.py

---
+++ function.py
@@ -0,0 +1,28 @@
+# Example
+
+# Print the string .
+
+# Input Format
+
+# The first line contains an integer .
+
+# Constraints
+
+
+# Output Format
+
+# Print the list of integers from  through  as a string, without spaces.
+
+# Sample Input 0
+
+# 3
+# Sample Output 0
+
+# 123
+if __name__ == '__main__':
+    n = int(input())
+    
+    for x in range(1,n+1):
+        print(x,end="")
+        
+      
